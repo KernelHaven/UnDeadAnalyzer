29e6a5cf454c1481526ab4c6f41ccfbfa2ac0a26
Adam <adam@sse.uni-hildesheim.de>
2017-10-05 15:15:28 +0200

    Port MissingAnalysis to an AnalysisComponent
    Also rename it to MissingVariablesFinder

--- src/net/ssehub/kernel_haven/default_analyses/MissingVariablesFinder.java
+++ src/net/ssehub/kernel_haven/default_analyses/MissingVariablesFinder.java
@@ -1,19 +1,19 @@
 package net.ssehub.kernel_haven.default_analyses;
 
 import java.io.File;
-import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import net.ssehub.kernel_haven.SetUpException;
-import net.ssehub.kernel_haven.analysis.AbstractAnalysis;
+import net.ssehub.kernel_haven.analysis.AnalysisComponent;
 import net.ssehub.kernel_haven.build_model.BuildModel;
 import net.ssehub.kernel_haven.code_model.CodeElement;
 import net.ssehub.kernel_haven.code_model.SourceFile;
 import net.ssehub.kernel_haven.config.Configuration;
-import net.ssehub.kernel_haven.util.BlockingQueue;
 import net.ssehub.kernel_haven.util.logic.Conjunction;
 import net.ssehub.kernel_haven.util.logic.Disjunction;
 import net.ssehub.kernel_haven.util.logic.Formula;
@@ -23,34 +23,50 @@ import net.ssehub.kernel_haven.variability_model.VariabilityModel;
 import net.ssehub.kernel_haven.variability_model.VariabilityVariable;
 
 /**
- * This Analysis uses a VariabilityVariable an looks in the code and build model to see if it 
- * is used. If it is not used it will write the unused variables in a file.
+ * This Analysis uses a VariabilityVariable an looks in the code and build model to see if it is used. If it is not used
+ * it will write the unused variables in a file.
  * 
  * @author Johannes
  * @author Manu
  * @author Adam
  */
-public class MissingAnalysis extends AbstractAnalysis {
+public class MissingVariablesFinder extends AnalysisComponent<String> {
+
+    private AnalysisComponent<VariabilityModel> vmComponent;
+
+    private AnalysisComponent<BuildModel> bmComponent;
+
+    private AnalysisComponent<SourceFile> cmComponent;
 
     /**
      * The different types of missing analyzes.
      */
     private enum Analysis {
-        DEFINED_BUT_NOT_USED,
-        USED_BUT_NOT_DEFINED,
+        DEFINED_BUT_NOT_USED, USED_BUT_NOT_DEFINED,
     }
-    
+
     private Analysis analyse;
-    
+
     /**
      * Default Constructor.
+     * 
      * @param config
      *            This analysis Properties are not extended. Never <code>null</code>.
-     * @throws SetUpException if analysis.missing.type is wrong.
+     * @param vmComponent
+     *            The component to provide the variability model.
+     * @param bmComponent
+     *            The component to provide the build model.
+     * @param cmComponent
+     *            The component to provide the code model.
+     * 
+     * @throws SetUpException
+     *             if analysis.missing.type is wrong.
      */
-    public MissingAnalysis(Configuration config) throws SetUpException {
+    public MissingVariablesFinder(Configuration config, AnalysisComponent<VariabilityModel> vmComponent,
+            AnalysisComponent<BuildModel> bmComponent, AnalysisComponent<SourceFile> cmComponent)
+            throws SetUpException {
         super(config);
-        
+
         String type = config.getProperty("analysis.missing.type", "D");
         if (type.equalsIgnoreCase("D")) {
             analyse = Analysis.DEFINED_BUT_NOT_USED;
@@ -59,57 +75,40 @@ public class MissingAnalysis extends AbstractAnalysis {
         } else {
             throw new SetUpException("analysis.missing.type is wrong: " + type);
         }
-        
+
     }
 
     @Override
-    public void run() {
-        LOGGER.logInfo("Start missing analysis");
-        try {
-            // Start all
-            vmProvider.start();
-            bmProvider.start();
-            cmProvider.start();
+    protected void execute() {
+        VariabilityModel vm = vmComponent.getNextResult();
+        BuildModel bm = bmComponent.getNextResult();
 
-            // Set all needed Models
-            VariabilityModel vm = null;
-            BuildModel bm = null;
-            vm = vmProvider.getResult();
-            bm = bmProvider.getResult();
-            
-            // Analysis
-            String filename = "";
-            Set<String> variables = null;
-            
-            switch (analyse) {
-            case DEFINED_BUT_NOT_USED:
-                LOGGER.logInfo("Defined but unused analysis");
-                variables = definedButUnused(vm, bm, cmProvider.getResultQueue());
-                filename = "definedButUnused.variables";
-                break;
-                
-            case USED_BUT_NOT_DEFINED:
-                LOGGER.logInfo("Used but not defined analysis");
-                variables = usedButNotDefined(vm, bm, cmProvider.getResultQueue());
-                filename = "usedButUndefined.variables";
-                break;
-                
-            default:
-                LOGGER.logError("Wrong configuration: no analysis defined");
-                break;
-            }
-            
-            // Put Result
-            PrintStream fw = createResultStream(filename);
-            for (String entry : variables) {
-                fw.println(entry);
-            }
-            fw.close();
-            
-        } catch (SetUpException e) {
-            LOGGER.logException("Provider failed", e);
+        List<SourceFile> cm = new LinkedList<>();
+        SourceFile file;
+        while ((file = cmComponent.getNextResult()) != null) {
+            cm.add(file);
         }
-        LOGGER.logInfo("Missing analysis done");
+
+        Set<String> variables = null;
+        switch (analyse) {
+        case DEFINED_BUT_NOT_USED:
+            LOGGER.logInfo("Defined but unused analysis");
+            variables = definedButUnused(vm, bm, cm);
+            break;
+
+        case USED_BUT_NOT_DEFINED:
+            LOGGER.logInfo("Used but not defined analysis");
+            variables = usedButNotDefined(vm, bm, cm);
+            break;
+
+        default:
+            LOGGER.logError("Wrong configuration: no analysis defined");
+            break;
+        }
+        
+        for (String variable : variables) {
+            addResult(variable);
+        }
     }
 
     /**
@@ -121,10 +120,9 @@ public class MissingAnalysis extends AbstractAnalysis {
      *            The build to search in. Never <code>null</code>.
      * @param files
      *            The files to search in. Never <code>null</code>.
-     * @return variables The set of variables, flagged with true or false
-     *         whether if the variable is used in the code.
+     * @return variables The set of variables, flagged with true or false whether if the variable is used in the code.
      */
-    public Set<String> definedButUnused(VariabilityModel vm, BuildModel bm, BlockingQueue<SourceFile> files) {
+    public Set<String> definedButUnused(VariabilityModel vm, BuildModel bm, List<SourceFile> files) {
         // Fill a map
         Map<String, Boolean> variables = new HashMap<>();
         for (VariabilityVariable variabilityVariable : vm.getVariables()) {
@@ -143,7 +141,7 @@ public class MissingAnalysis extends AbstractAnalysis {
             for (CodeElement element : file) {
                 Set<String> names = new HashSet<>();
                 getVariableNamesInElement(element, names);
-                
+
                 for (String var : names) {
                     variables.put(var, true);
                 }
@@ -157,7 +155,7 @@ public class MissingAnalysis extends AbstractAnalysis {
         }
         return definedButUnused;
     }
-    
+
     /**
      * Searching for defined but unused Variables.
      * 
@@ -167,10 +165,9 @@ public class MissingAnalysis extends AbstractAnalysis {
      *            The build to search in. Never <code>null</code>.
      * @param files
      *            The files to search in. Never <code>null</code>.
-     * @return variables The set of variables, flagged with true or false
-     *         whether if the variable is used in the code.
+     * @return variables The set of variables, flagged with true or false whether if the variable is used in the code.
      */
-    public Set<String> usedButNotDefined(VariabilityModel vm, BuildModel bm, BlockingQueue<SourceFile> files) {
+    public Set<String> usedButNotDefined(VariabilityModel vm, BuildModel bm, List<SourceFile> files) {
         Map<String, Boolean> variables = new HashMap<>();
         // Fill a map with build model
         for (File file : bm) {
@@ -208,23 +205,25 @@ public class MissingAnalysis extends AbstractAnalysis {
     }
 
     /**
-     * Recursively finds all variable names that start with CONFIG_
-     * in the presence conditions of an element and all child elements.
-     *  
-     * @param element The element to search in.
-     * @param result The resulting set of variable names
+     * Recursively finds all variable names that start with CONFIG_ in the presence conditions of an element and all
+     * child elements.
+     * 
+     * @param element
+     *            The element to search in.
+     * @param result
+     *            The resulting set of variable names
      */
     private void getVariableNamesInElement(CodeElement element, Set<String> result) {
         getVariableNamesInFormula(result, element.getPresenceCondition());
-        
+
         for (CodeElement child : element.iterateNestedElements()) {
             getVariableNamesInElement(child, result);
         }
     }
 
     /**
-     * Recursively fills a set with strings with names of variables from a Formula. Only
-     * variables that start with CONFIG_ are considered.
+     * Recursively fills a set with strings with names of variables from a Formula. Only variables that start with
+     * CONFIG_ are considered.
      * 
      * @param formular
      *            The Formula to check. Never <code>null</code>.
@@ -250,5 +249,5 @@ public class MissingAnalysis extends AbstractAnalysis {
             getVariableNamesInFormula(names, neg.getFormula());
         }
     }
-    
+
 }
