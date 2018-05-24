37eb2d7667c32d6ca452075e0525859d9412d390
moritz <moritzf@gmail.com>
2018-05-24 14:51:29 +0200

    Add ThreadedDeadCodeAnalysis

---
+++ src/net/ssehub/kernel_haven/undead_analyzer/ThreadedDeadCodeAnalysis.java
@@ -0,0 +1,26 @@
+package net.ssehub.kernel_haven.undead_analyzer;
+
+import net.ssehub.kernel_haven.SetUpException;
+import net.ssehub.kernel_haven.analysis.AnalysisComponent;
+import net.ssehub.kernel_haven.analysis.PipelineAnalysis;
+import net.ssehub.kernel_haven.config.Configuration;
+import net.ssehub.kernel_haven.util.null_checks.NonNull;
+
+public class ThreadedDeadCodeAnalysis extends PipelineAnalysis {
+
+	/**
+	 * Creates this analysis.
+	 * 
+	 * @param config
+	 *            The global configuration.
+	 */
+	public ThreadedDeadCodeAnalysis(@NonNull Configuration config) {
+		super(config);
+	}
+
+	@Override
+	protected @NonNull AnalysisComponent<?> createPipeline() throws SetUpException {
+		return new ThreadedDeadCodeFinder(config, getVmComponent(), getBmComponent(), getCmComponent());
+	}
+
+}
