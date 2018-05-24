36cb923ccbe7d3559fb372d333f66a023f8f5114
moritz <moritzf@gmail.com>
2018-05-24 14:39:11 +0200

    Make number of threads configurable

--- src/net/ssehub/kernel_haven/undead_analyzer/ThreadedDeadCodeFinder.java
+++ src/net/ssehub/kernel_haven/undead_analyzer/ThreadedDeadCodeFinder.java
@@ -4,6 +4,7 @@ import static net.ssehub.kernel_haven.util.null_checks.NullHelpers.notNull;
 
 import java.util.List;
 
+import net.ssehub.kernel_haven.SetUpException;
 import net.ssehub.kernel_haven.analysis.AnalysisComponent;
 import net.ssehub.kernel_haven.build_model.BuildModel;
 import net.ssehub.kernel_haven.cnf.VmToCnfConverter;
@@ -21,21 +22,25 @@ import net.ssehub.kernel_haven.variability_model.VariabilityModel;
  */
 public class ThreadedDeadCodeFinder extends DeadCodeFinder {
 
-    private static final int NUM_THREADS = 8;
     
-    /**
+    private @NonNull Configuration config;
+
+	/**
      * Creates a dead code analysis.
      *  
      * @param config The user configuration; not used.
      * @param vmComponent The component to provide the variability model.
      * @param bmComponent The component to provide the build model.
      * @param cmComponent The component to provide the code model.
+	 * @throws SetUpException 
      */
     public ThreadedDeadCodeFinder(@NonNull Configuration config,
             @NonNull AnalysisComponent<VariabilityModel> vmComponent,
-            @NonNull AnalysisComponent<BuildModel> bmComponent, @NonNull AnalysisComponent<SourceFile> cmComponent) {
-        
+            @NonNull AnalysisComponent<BuildModel> bmComponent, @NonNull AnalysisComponent<SourceFile> cmComponent) throws SetUpException {
         super(config, vmComponent, bmComponent, cmComponent);
+        
+    	this.config = config;
+    	UndeadSettings.registerAllSettings(config);
         
     }
 
@@ -56,12 +61,18 @@ public class ThreadedDeadCodeFinder extends DeadCodeFinder {
                 relevancyChecker = new FormulaRelevancyChecker(vm, considerVmVarsOnly);
             }
             
+            int threadCount = config.getValue(UndeadSettings.NUMBER_OF_OF_THREADS);
+            if (threadCount < 1) {
+            	LOGGER.logWarning("Thread number was <1, falling back to using 1 thread.");
+            	threadCount = 1;
+            }
+            
             OrderPreservingParallelizer<SourceFile, List<@NonNull DeadCodeBlock>> parallelizer
                 = new OrderPreservingParallelizer<>(this::findDeadCodeBlocks, (deadBlocks) -> {
                     for (DeadCodeBlock block : deadBlocks) {
                         addResult(block);
                     }
-                }, NUM_THREADS);
+                }, threadCount);
             
             SourceFile file;
             while ((file = cmComponent.getNextResult()) != null) {
